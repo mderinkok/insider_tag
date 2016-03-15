a1ca4a7060647a89bb77a82105d9ea0119bf2089
mderinkok <muharrem@useinsider.com>
2016-03-15 19:15:21 +0200

    Create integration.js

---
+++ integration.js
@@ -0,0 +1,14 @@
+<script type='text/javascript'>
+    var _spapi = _spapi || [];
+    _spapi.push(['_partner', 'demo']);
+    (
+        function()
+        {
+            var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+            ga.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'demo.api.sociaplus.com/partner.js';
+            var s = document.getElementsByTagName('script')[0];
+            s.parentNode.insertBefore(ga, s);
+        }
+    )();
+</script>
+          
